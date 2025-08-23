148cd6e53162ed2fd446a37b95aab239c57f8884
Henry M. <henrydev10@gmail.com>
2025-08-23 18:12:36 +0300

    trying to debug the issue of new files are not refreshed as unversioned

--- src/main/java/org/genrym/autovcsrefresh/AutoVcsRefreshStartup.java
+++ src/main/java/org/genrym/autovcsrefresh/AutoVcsRefreshStartup.java
@@ -1,17 +1,26 @@
 package org.genrym.autovcsrefresh;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.ChangesViewManager;
+import com.intellij.openapi.vcs.changes.InvokeAfterUpdateMode;
 import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager;
 import com.intellij.openapi.vfs.AsyncFileListener;
 import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class AutoVcsRefreshStartup implements StartupActivity {
 
+    private static final Logger LOG = Logger.getInstance(AutoVcsRefreshStartup.class);
+
     @Override
     public void runActivity(@NotNull Project project) {
+        LOG.info("AutoVcsRefresh initialized for project: " + project.getName());
+
         VirtualFileManager.getInstance().addAsyncFileListener(
                 events -> {
                     // If any file was changed externally, trigger a refresh
@@ -20,11 +29,36 @@ public class AutoVcsRefreshStartup implements StartupActivity {
                             @Override
                             public void afterVfsChange() {
                                 var dirtyScopeManager = VcsDirtyScopeManager.getInstance(project);
+                                var changeListManager = ChangeListManager.getInstance(project);
+                                var changesViewManager = ChangesViewManager.getInstance(project);
+
                                 for (VFileEvent event : events) {
                                     var file = event.getFile();
                                     if (file != null) {
-                                        // Mark the specific file as dirty
-                                        dirtyScopeManager.fileDirty(file);
+                                        if (changeListManager.isIgnoredFile(file)) {
+                                            LOG.info(String.format("AutoVcsRefresh: file is ignored by VCS: %s", file.getPath()));
+                                            continue;
+                                        }
+                                        if (event instanceof VFileCreateEvent) {
+                                            LOG.info(String.format("AutoVcsRefresh: new file was created: %s", file.getPath()));
+                                            // Mark the parent dir as dirty - to search new files as unversioned
+                                            var parent = file.getParent();
+                                            if (parent != null) {
+                                                LOG.info(String.format("AutoVcsRefresh: marking directory dirty: %s", parent.getPath()));
+                                                dirtyScopeManager.dirDirtyRecursively(parent);
+                                                changeListManager.invokeAfterUpdate(() -> {
+                                                    LOG.info("ChangeListManager update completed for file: " + file.getPath());
+                                                    // Refresh Unversioned Files view
+                                                    LOG.info("Scheduled refresh for Unversioned Files view");
+                                                    changesViewManager.scheduleRefresh();
+                                                }, InvokeAfterUpdateMode.SILENT, "AutoVcsRefresh", null);
+                                            } else {
+                                                LOG.info("AutoVcsRefresh: new file was created but no parent directory");
+                                            }
+                                        } else {
+                                            LOG.info(String.format("AutoVcsRefresh: existing file was changed: %s", file.getPath()));
+                                            dirtyScopeManager.fileDirty(file); // Mark the specific file as dirty
+                                        }
                                     }
                                 }
                             }
